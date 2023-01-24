06eb94a8eeb97fa4892c0e3a73f0a55df5c08e71
Ciszkoo <pawel.olszko.p@gmail.com>
2023-01-24 13:56:52 +0100

    client: update login and register form

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -3,14 +3,22 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { useAuth } from "../context/AuthProvider";
+import Card from "./Card/Card";
+import Button from "./Button/Button";
+import { useState } from "react";
 
 interface LoginFormProps {
   setIsRegister: () => void;
 }
 
 const LoginSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(8).max(50),
+  email: z
+    .string({ required_error: "Email jest wymagany" })
+    .email({ message: "Podaj poprawny email" }),
+  password: z
+    .string({ required_error: "Hasło jest wymagane" })
+    .min(8, { message: "Hasło musi mieć min. 8 znaków" })
+    .max(50, { message: "Hasło może mieć max. znaków" }),
 });
 
 type LoginFormSchemaType = z.infer<typeof LoginSchema>;
@@ -24,34 +32,54 @@ const LoginForm = (props: LoginFormProps) => {
 
   const { handleLogin } = useAuth();
 
-  const onSubmit: SubmitHandler<LoginFormSchemaType> = (data) =>
+  const onSubmit: SubmitHandler<LoginFormSchemaType> = (data) => {
     handleLogin(data);
+  };
 
   return (
-    <form
-      className="flex flex-col flex-auto justify-center items-center self-center"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <div className="flex flex-col">
-        <input
-          className={`input-sign ${errors.email && "invalid"}`}
-          placeholder="Adres e-mail"
-          {...register("email")}
-          disabled={isSubmitting}
-        />
-        <input
-          className={`input-sign ${errors.password && "invalid"}`}
-          placeholder="Hasło"
-          {...register("password")}
-          disabled={isSubmitting}
-          type="password"
-        />
-        <button type="submit">Zaloguj</button>
+    <Card customClass="self-center my-auto p-20 relative">
+      <form
+        className="flex flex-col flex-auto justify-center items-center self-center"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="flex flex-col">
+          <input
+            className={`input-sign ${errors.email && "invalid"}`}
+            placeholder="Adres e-mail"
+            {...register("email")}
+            disabled={isSubmitting}
+          />
+          <input
+            className={`input-sign ${errors.password && "invalid"}`}
+            placeholder="Hasło"
+            {...register("password")}
+            disabled={isSubmitting}
+            type="password"
+          />
+        </div>
+        <div className="flex gap-2 mt-1">
+          <Button type="submit" circle={false} lightness="300">
+            Zaloguj
+          </Button>
+          <Button
+            type="button"
+            circle={false}
+            lightness="300"
+            handleOnClick={props.setIsRegister}
+          >
+            Utwórz konto
+          </Button>
+        </div>
+      </form>
+      <div className="absolute bottom-2">
+        {errors.email && (
+          <p className="text-rose-500 text-xs">{errors.email.message}</p>
+        )}
+        {errors.password && (
+          <p className="text-rose-500 text-xs">{errors.password.message}</p>
+        )}
       </div>
-      <button type="button" onClick={props.setIsRegister}>
-        Utwórz konto
-      </button>
-    </form>
+    </Card>
   );
 };
 
--- client/src/components/RegisterForm.tsx
+++ client/src/components/RegisterForm.tsx
@@ -2,23 +2,45 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
+import Card from "./Card/Card";
+import Button from "./Button/Button";
+import { useState } from "react";
 
 interface RegisterFormProps {
   setIsRegister: () => void;
 }
 
 const RegisterSchema = z.object({
-  firstName: z.string().min(2).max(50),
-  lastName: z.string().min(2).max(50),
-  email: z.string().email(),
-  password: z.string().min(8).max(50),
-  dateOfBirth: z.string(), // TODO: z.date()
+  firstName: z
+    .string({ required_error: "Podaj imię" })
+    .min(2, { message: "Imię musi mieć min. 2 znaki" })
+    .max(50, { message: "Imię może mieć max. 50 znaków" }),
+  lastName: z
+    .string({ required_error: "Podaj nazwisko" })
+    .min(2, { message: "Nazwisko musi mieć min. 2 znaki" })
+    .max(50, { message: "Nazwisko może mieć max. 50 znaków" }),
+  email: z
+    .string({ required_error: "Podaj email" })
+    .email({ message: "Podaj poprawny email" }),
+  password: z
+    .string({ required_error: "Podaj hasło" })
+    .min(8, { message: "Hasło musi mieć min. 8 znaków" })
+    .max(50, { message: "Hasło może mieć max. 50 znaków" }),
+  dateOfBirth: z
+    .date({
+      required_error: "Wybierz datę urodzenia",
+      invalid_type_error: "Wybierz datę urodzenia",
+    })
+    .min(new Date("1900-01-01"), { message: "Chyba jesteś trochę za stary" })
+    .max(new Date(), { message: "Chyba jesteś trochę za młody" }),
   gender: z.enum(["male", "female", "other"]),
 });
 
 type RegisterFormSchemaType = z.infer<typeof RegisterSchema>;
 
 const RegisterForm = (props: RegisterFormProps) => {
+  const [registerFailed, setRegisterFailed] = useState<boolean>(false);
+
   const {
     register,
     formState: { errors, isSubmitting },
@@ -28,59 +50,94 @@ const RegisterForm = (props: RegisterFormProps) => {
   });
   const onSubmit: SubmitHandler<RegisterFormSchemaType> = async (data) => {
     try {
-      await axios.post("/api/user", data);
+      setRegisterFailed(false);
+      const mapped = {...data, dateOfBirth: data.dateOfBirth.toISOString().split("T")[0]}
+      await axios.post("/api/user", mapped);
       console.log("Account created");
       props.setIsRegister();
     } catch (error) {
+      setRegisterFailed(true);
       console.log(error);
     }
   };
 
   return (
-    <form
-      className="flex flex-col flex-auto justify-center items-center"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <button onClick={props.setIsRegister}>&lt;&lt; Powrót </button>
-      <input
-        className={`input-sign ${errors.firstName && "invalid"}`}
-        placeholder="Imię"
-        {...register("firstName")}
-        disabled={isSubmitting}
-      />
-      <input
-        className={`input-sign ${errors.lastName && "invalid"}`}
-        placeholder="Nazwisko"
-        {...register("lastName")}
-        disabled={isSubmitting}
-      />
-      <input
-        className={`input-sign ${errors.email && "invalid"}`}
-        placeholder="Adres e-mail"
-        {...register("email")}
-        disabled={isSubmitting}
-      />
-      <input
-        className={`input-sign ${errors.password && "invalid"}`}
-        placeholder="Hasło"
-        {...register("password")}
-        disabled={isSubmitting}
-        type="password"
-      />
-      <input
-        className={`input-sign ${errors.dateOfBirth && "invalid"}`}
-        placeholder="Data urodzenia"
-        {...register("dateOfBirth")}
-        disabled={isSubmitting}
-        type="date"
-      />
-      <select className="input-sign" {...register("gender")}>
-        <option value="male">male</option>
-        <option value="female">female</option>
-        <option value="other">other</option>
-      </select>
-      <button type="submit">Zarejestruj</button>
-    </form>
+    <Card customClass="self-center my-auto p-20 relative">
+      <form
+        className="flex flex-col flex-auto justify-center items-center gap-1"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <Button
+          lightness="300"
+          circle={false}
+          handleOnClick={props.setIsRegister}
+          customClass="absolute top-2 right-2"
+        >
+          &lt;&lt; Powrót{" "}
+        </Button>
+        <input
+          className={`input-sign ${errors.firstName && "invalid"}`}
+          placeholder="Imię"
+          {...register("firstName")}
+          disabled={isSubmitting}
+        />
+        <input
+          className={`input-sign ${errors.lastName && "invalid"}`}
+          placeholder="Nazwisko"
+          {...register("lastName")}
+          disabled={isSubmitting}
+        />
+        <input
+          className={`input-sign ${errors.email && "invalid"}`}
+          placeholder="Adres e-mail"
+          {...register("email")}
+          disabled={isSubmitting}
+        />
+        <input
+          className={`input-sign ${errors.password && "invalid"}`}
+          placeholder="Hasło"
+          {...register("password")}
+          disabled={isSubmitting}
+          type="password"
+        />
+        <select className="input-sign w-full" {...register("gender")}>
+          <option value="male">Mężczyzna</option>
+          <option value="female">Kobieta</option>
+          <option value="other">Inna płeć</option>
+        </select>
+        <input
+          className={`input-sign ${errors.dateOfBirth && "invalid"}`}
+          placeholder="Data urodzenia"
+          {...register("dateOfBirth", { valueAsDate: true })}
+          disabled={isSubmitting}
+          type="date"
+          max={new Date().toISOString().split("T")[0]}
+        />
+        <Button type="submit" circle={false} lightness="300">
+          Zarejestruj
+        </Button>
+      </form>
+      <div className="absolute bottom-2">
+        {registerFailed && (
+          <p className="text-rose-500 text-xs">Email jest już zajęty!</p> 
+        )}
+        {errors.firstName && (
+          <p className="text-rose-500 text-xs">{errors.firstName.message}</p>
+        )}
+        {errors.lastName && (
+          <p className="text-rose-500 text-xs">{errors.lastName.message}</p>
+        )}
+        {errors.email && (
+          <p className="text-rose-500 text-xs">{errors.email.message}</p>
+        )}
+        {errors.password && (
+          <p className="text-rose-500 text-xs">{errors.password.message}</p>
+        )}
+        {errors.dateOfBirth && (
+          <p className="text-rose-500 text-xs">{errors.dateOfBirth.message}</p>
+        )}
+      </div>
+    </Card>
   );
 };
 
